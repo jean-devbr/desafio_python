16901c2ba1ac6e237641d4e59b990eb5c49dcf11
jean costa <costajean1005@gmail.com>
2025-05-14 11:05:41 -0300

    desafio 034
    terceiro

---
+++ Nivel 3/034/034(3).py
@@ -0,0 +1,12 @@
+# Terceiro
+x = 7
+y = -1
+z = (x * y) + 5 
+if z <= 0:
+    resposta = "A"
+elif z <= 100:
+    resposta = "B"
+else:
+    resposta = "C"
+
+print(f'O resultado de z é {z} e o respostado é {resposta}')
