23aca671e9923be69177943283e08beca726d96f
jean costa <costajean1005@gmail.com>
2025-05-14 11:09:36 -0300

    desafio 034
    quarto

---
+++ Nivel 3/034/034(4).py
@@ -0,0 +1,12 @@
+# Quarto
+x = -2
+y = 5 
+z = (x * y) + 5 
+if z <= 0:
+    resposta = "A"
+elif z <= 100:
+    resposta = "B"
+else:
+    resposta = "C"
+
+print(f'O resultado de z é {z} e a resposta é {resposta}')
