db9c5fb29eb06d5291b18374618ed440d64197dc
jean costa <costajean1005@gmail.com>
2025-05-14 10:57:23 -0300

    desafio 034
    primeira

---
+++ Nivel 3/034/034(1).py
@@ -0,0 +1,12 @@
+# Primeiro
+x = 3
+y = 2 
+z = (x * y) + 5
+if z <= 0:
+    resposta = "A"
+elif z <= 100:
+    resposta = "B"
+else:
+    resposta = "C"
+
+print(f'O valor de z é {z} e a resposta é {resposta}')
