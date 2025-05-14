a92396fa0b9e03572821739db151bd9830075bcd
jean costa <costajean1005@gmail.com>
2025-05-14 11:13:50 -0300

    desafio 034
    quinta (ultimo)

---
+++ Nivel 3/034/034(5).py
@@ -0,0 +1,13 @@
+# Quinto (ultimo)
+
+x = 50
+y = 3
+z = ( x * y) + 5
+if z <= 0:
+    resposta = "A"
+elif z <= 100:
+    resposta = "B"
+else:
+    resposta = "C"
+
+print(f'O resultado de z é {z} e a resposta é {resposta}')
