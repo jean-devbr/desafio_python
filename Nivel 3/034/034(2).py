1b3f1023ebde6e2128bb009e2610a2947ba8dbfc
jean costa <costajean1005@gmail.com>
2025-05-14 11:00:37 -0300

    desafio 034
    segunda

---
+++ Nivel 3/034/034(2).py
@@ -0,0 +1,11 @@
+# Segundo
+x = 150
+y = 3
+z = ( x * y) + 5 
+if z <= 0:
+    resposta = "A"
+elif z <= 100:
+    resposta = "B"
+else:
+    resposta = "C"
+print(f'O resultado de z é {z} e a resposta é {resposta}')
