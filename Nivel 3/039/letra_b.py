22cc08a559f167b082e7971be507a96a84c35017
jean costa <costajean1005@gmail.com>
2025-05-22 09:31:33 -0300

    desafio 039
    letra b

---
+++ Nivel 3/039/letra_b.py
@@ -0,0 +1,7 @@
+# Para A = V, B = V e C = F, qual o resultado da avaliação das seguintes expressões:
+#letra B: (A ou B) e (A e C) 
+A = True
+B = True
+C = False
+resultado = (A or B) and (A and C)
+print(resultado)
