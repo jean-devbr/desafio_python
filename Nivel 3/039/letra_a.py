f902067c1361bf7d2688d1cd1ba454915bce836e
jean costa <costajean1005@gmail.com>
2025-05-22 09:25:51 -0300

    desafio 039
    letra a

---
+++ Nivel 3/039/letra_a.py
@@ -0,0 +1,6 @@
+# Para A = V, B = V e C = F, qual o resultado da avaliação das seguintes expressões:
+#letra A: (A e B) ou (A xou B) 
+A = True
+B = True
+resultado = (A and B) or (A != B)
+print(resultado)
