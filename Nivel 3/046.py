fbdca4786d63b274f469127974a1bd5a1393964d
jean costa <costajean1005@gmail.com>
2025-05-28 08:49:59 -0300

    desafio 046

---
+++ Nivel 3/046.py
@@ -0,0 +1,12 @@
+# Acrescentar uma mensagem de 'VALOR INVÁLIDO' no exercício [44] caso o segundo valor informado seja ZERO
+try:
+    numero1 = float(input("Digite o primeiro número: "))
+    numero2 = float(input("Digite o segundo número: "))
+    while numero2 == 0:
+        numero2 = float(input("VALOR INVÁLIDO! Não pode ser zero. \nDigite novamente o segundo número: "))
+        break
+    divisao = numero1 / numero2
+    print(f'A divisão entre {numero1} e {numero2} é : {divisao:.2f}')
+
+except ValueError:
+    print("Erro! Por favor, digite apenas números reais.")
