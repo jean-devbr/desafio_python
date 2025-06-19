73e13d08e1ad6fe0ca2e69f157f2f9b58d0be99d
jean costa <costajean1005@gmail.com>
2025-06-19 10:44:38 -0300

    Desafio 067
    Otimo desafio!!

---
+++ Nivel 3/067.py
@@ -0,0 +1,8 @@
+# Faça um algoritmo que calcule e escreva a média aritmética dos números inteiros entre 15 (inclusive) e 100 (inclusive). 
+soma = 0
+contador = 0
+for i in range(15,101):
+    soma += i
+    contador += 1
+media = soma / contador
+print(media)
